040c0dce50eb45de24d48b7eb46f679f7eb56fca
Dananjaya Sandakalum <68169940+hgdsandakalum@users.noreply.github.com>
2021-10-10 10:21:17 +0530

    adding states in home page

--- train_srilanka/lib/Screens/home_page.dart
+++ train_srilanka/lib/Screens/home_page.dart
@@ -6,10 +6,30 @@ import 'package:train_srilanka/widgets/navbar.dart';
 import '../theme.dart';
 import 'UserAuth/register.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   final ButtonStyle styleBtnSubmit = ElevatedButton.styleFrom(
     textStyle: const TextStyle(fontSize: 20, backgroundColor: kPrimaryColor),
   );
+
+  bool viewVisible = false;
+
+  void handleWidgets() {
+    if (viewVisible) {
+      setState(() {
+        viewVisible = false;
+      });
+      ;
+    } else {
+      setState(() {
+        viewVisible = true;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,107 +37,117 @@ class Homepage extends StatelessWidget {
         drawer: NavDrawer(),
         appBar: CustomAppBar(),
         body: SafeArea(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                'Search for Train Schedules',
-                style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: kPrimaryColor),
-                textAlign: TextAlign.left,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none, hintText: 'I am at'),
-                      keyboardType: TextInputType.emailAddress,
-                      textInputAction: TextInputAction.next,
-                    ),
-                  ),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: <
+                    Widget>[
+          SizedBox(height: 30.0),
+          Text(
+            'Search for Train Schedules',
+            style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: kPrimaryColor),
+            textAlign: TextAlign.left,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: 'I am at'),
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
                 ),
               ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'I want to go to'),
-                      keyboardType: TextInputType.emailAddress,
-                      textInputAction: TextInputAction.next,
-                    ),
-                  ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: 'I want to go to'),
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
                 ),
               ),
-              Container(
-                  margin:
+            ),
+          ),
+          Visibility(
+              maintainSize: false,
+              maintainAnimation: true,
+              maintainState: true,
+              visible: viewVisible,
+              child: Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.green,
+                  margin: EdgeInsets.only(top: 50, bottom: 30),
+                  child: )),
+          Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              width: double.infinity,
+              child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                        flex: 1,
+                        child: ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                                primary: kSecondaryColor,
+                                textStyle: const TextStyle(fontSize: 19),
+                                padding: EdgeInsets.symmetric(vertical: 10.0)),
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.compare_arrows_sharp,
+                              size: 26.0,
+                            ),
+                            label: const Text('REVERSE'))),
+                    Container(width: 50),
+                    Expanded(
+                        flex: 1,
+                        child: ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                                primary: kSecondaryColor,
+                                textStyle: const TextStyle(fontSize: 19),
+                                padding: EdgeInsets.symmetric(vertical: 10.0)),
+                            onPressed: handleWidgets,
+                            icon: Icon(
+                              Icons.arrow_drop_down_sharp,
+                              size: 26.0,
+                            ),
+                            label: const Text('MORE')))
+                  ])),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            width: double.infinity,
+            child: ElevatedButton(
+              child: Padding(
+                  padding:
                       EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  width: double.infinity,
-                  child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Expanded(
-                            flex: 1,
-                            child: ElevatedButton.icon(
-                                style: ElevatedButton.styleFrom(
-                                    primary: kSecondaryColor,
-                                    textStyle: const TextStyle(fontSize: 19),
-                                    padding: EdgeInsets.symmetric(
-                                        vertical: 10.0, horizontal: 25.0)),
-                                onPressed: () {},
-                                icon: const Icon(Icons.compare_arrows_sharp),
-                                label: const Text('MORE'))),
-                        Container(width: 50),
-                        Expanded(
-                            flex: 1,
-                            child: ElevatedButton.icon(
-                                style: ElevatedButton.styleFrom(
-                                    primary: kSecondaryColor,
-                                    textStyle: const TextStyle(fontSize: 19)),
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.arrow_drop_down_sharp,
-                                  size: 13.0,
-                                ),
-                                label: const Text('MORE')))
-                      ])),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                width: double.infinity,
-                child: ElevatedButton(
-                  child: Padding(
-                      padding: EdgeInsets.symmetric(
-                          vertical: 10.0, horizontal: 25.0),
-                      child: const Text('SEARCH')),
-                  style: ElevatedButton.styleFrom(
-                      primary: kPrimaryColor,
-                      textStyle: const TextStyle(fontSize: 19)),
-                  onPressed: () {
-                    print('Pressed');
-                  },
-                ),
-              )
-            ])));
+                  child: const Text('SEARCH')),
+              style: ElevatedButton.styleFrom(
+                  primary: kPrimaryColor,
+                  textStyle: const TextStyle(fontSize: 19)),
+              onPressed: () {
+                print('Pressed');
+              },
+            ),
+          )
+        ])));
   }
 }
